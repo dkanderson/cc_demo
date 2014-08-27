1f278ba445f45e412e9fc833ab87b65418f035cc
dkanderson <dkanderson.designs@gmail.com>
2014-08-27 19:12:09 -0400

    add video resize script, fix typos and stupidity

--- js/script.js
+++ js/script.js
@@ -2,11 +2,37 @@
 (function(){
 
 	var App = {
-		Models = {},
-		Collections = {},
-		Views = {},
-		Routes = {}
+		Models:{},
+		Collections: {},
+		Views:{},
+		Routes: {}
 	};
+
+  var allVideos = $('iframe'), fluidEl = $("figure");
+        
+    allVideos.each(function() {
+  
+    $(this)
+      // jQuery .data does not work on object/embed elements
+      .attr('data-aspectRatio', this.height / this.width)
+      .removeAttr('height')
+      .removeAttr('width');
+  
+  });
+  
+  $(window).resize(function() {
+  
+    var newWidth = fluidEl.width();
+    allVideos.each(function() {
+    
+      var el = $(this);
+      el
+          .width(newWidth)
+          .height(newWidth * el.attr('data-aspectRatio'));
+    
+    });
+  
+  }).resize();
 
 	//global template function
   window.template = function(id){
@@ -17,7 +43,7 @@
   window.vent = _.extend({}, Backbone.Events);
 
 	// Video model
-  App.Models.Video = Backbone.Models.extend({
+  App.Models.Video = Backbone.Model.extend({
 
 		defaults: {
 			thumbnail: 'img/thumb.jpg'
